8c54f4cab2a1a14f90c51ec488d73c0dbfdadd12
Mihir Mishra <mihir.mishra988@gmail.com>
2024-08-04 22:18:55 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -4,14 +4,13 @@ Exceptions are used to handle errors and exceptions in the code.
 '''
 
 import sys
-from logger import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info() # get the exception traceback object
-    file_name=exc_tb.tb_frame.f_code.co_filename #exc_tb contains the traceback object
+    _,_,exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
     error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
-     file_name,exc_tb.tb_lineno,str(error)
-     )
+     file_name,exc_tb.tb_lineno,str(error))
 
     return error_message
 
@@ -24,5 +23,7 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
+    
 
-   
+
+        
